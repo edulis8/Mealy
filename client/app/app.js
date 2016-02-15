ffb24acff30f969e414839bbe03d08ee99ae23ab
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-02-15 15:45:27 -0800

    Complete form for cooks.

--- client/app/app.js
+++ client/app/app.js
@@ -22,4 +22,24 @@ $routeProvider
 
 .controller('HomeController', function($scope){
 
-});
+})
+
+
+.factory('Cook', function($http){
+
+  var postLink = function( link ){
+    console.log('post link', link);
+    //link = JSON.stringify(link);
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    }).then(function(resp){
+      console.log('Posted!', resp.data);
+      return resp.data;
+    });
+  };
+
+  return {postLink : postLink};
+
+})
